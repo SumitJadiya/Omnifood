d8ef9d2a458301713805b0cb736eebebc6cb8903
sumit.jadiya <sumit.jadiya@appdirect.com>
2020-04-08 20:10:50 +0530

    adds animation

--- resources/js/script.js
+++ resources/js/script.js
@@ -11,6 +11,7 @@ $(document).ready(function name(params) {
     }, {
         offset: '60px;'
     });
+
     /*        Scroll On Button          */
     $(".js--scroll-to-plans").click(function () {
         $('html, body').animate({ scrollTop: $(".js--section-plans").offset().top }, 1000);
@@ -19,5 +20,28 @@ $(document).ready(function name(params) {
     $(".js--scroll-to-start").click(function () {
         $('html, body').animate({ scrollTop: $(".js--section-features").offset().top }, 1000);
     })
+
+    /*       Animation on Scroll         */
+    $(".js--wp-1").waypoint(function (direction) {
+        $(".js--wp-1").addClass("animated fadeIn");
+    }, {
+        offset: "50%;"
+    })
+    $(".js--wp-2").waypoint(function (direction) {
+        $(".js--wp-2").addClass("animated fadeInUp");
+    }, {
+        offset: "50%;"
+    })
+    $(".js--wp-3").waypoint(function (direction) {
+        $(".js--wp-3").addClass("animated fadeIn");
+    }, {
+        offset: "50%;"
+    })
+    $(".js--wp-4").waypoint(function (direction) {
+        $(".js--wp-4").addClass("animated pulse");
+    }, {
+        offset: "50%;"
+    })
+
 
 });
